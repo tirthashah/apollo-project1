8ad8c45bb8edeef24a96651e7b24eb44527311b7
Tirtha <tirthashah201@gmail.com>
2025-09-16 18:08:43 +0530

    new chnages

---
+++ dist/run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting the LinkedIn Automation Bot...
+
+REM --- This is the command that runs your program ---
+name_gen.exe --account "Brijesh" --action "both" --file "results.xlsx"
+
+echo.
+echo --- Script has finished ---
+pause
